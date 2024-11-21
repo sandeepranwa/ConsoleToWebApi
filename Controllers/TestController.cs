a34ca101ddb9dfaf1a8fc4edf75f9a41d2ea700e
MK <MK@DESKTOP-UL430U1>
2024-11-21 21:10:34 +0530

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleToWebApi.Controllers
+{
+    
+    [ApiController]
+    [Route("test/[action]")]
+    public class TestController : ControllerBase
+    {
+        public string Get() {
+            return "Hello from get";
+        }
+
+        public string Get1()
+        {
+            return "Hello from get one";
+        }
+    }
+}
